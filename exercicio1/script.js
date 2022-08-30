0f9575b3d9a347ee0cb945e739b4b2d6c58b77f2
MatheusGravini <gravini.mello@gmail.com>
2022-08-30 16:20:48 -0300

    Exercicio resolvidos a008

---
+++ exercicio1/script.js
@@ -0,0 +1,34 @@
+
+//a)
+const numero = Number ( prompt("Digite um número par") ) 
+
+//b)
+console.log("O numero digitado dividido por 2 é:", numero / 2)
+
+console.log("O seu resto é:", numero % 2 )
+
+//c) O mesmo padrão se repete para todo e qualquer número par que o usuário inserir, retornando o resto 0.
+
+
+//d) Independente do número Ímpar escolhido pelo usuário, sempre retornará o resto 1.
+
+
+console.log (typeof numero)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
